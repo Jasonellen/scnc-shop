0300bf2c2e5f59ca7bbd92aee54ab4fbdd1b95aa
张南南 <269248791@qq.com>
2018-05-16 10:33:11 +0800

    切换手机bugfix
    xx

--- src/service/global.js
+++ src/service/global.js
@@ -64,6 +64,22 @@ if(location.href.indexOf('from') !== -1){
 	location.href = location.origin + location.pathname
 }
 
+var ua = window.navigator.userAgent.toLowerCase();  //切换手机
+if(ua.match(/MicroMessenger/i) == 'micromessenger'){
+	fetchJsonp(url.binding)
+		.then((response)=>{
+			return response.json()
+		})
+		.then((data)=>{
+			console.log(data,'successjsonp')
+			let oldToken = localStorage.getItem('s_token_old')
+			let nowToken = localStorage.getItem('s_token')
+			if((oldToken !== data.token) && (nowToken !== data.token)){
+				localStorage.setItem('s_token',data.token)
+				location.reload()
+			}
+		})
+}
 //扩展对象
 if (typeof Object.assign != 'function') {
 	Object.assign = function(target) {
